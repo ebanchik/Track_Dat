cd620ae90c70552783040d02443daa8cc92e8f6a
ebanchik <ebanchik@gmail.com>
2023-12-28 17:24:08 -0500

    some styling stuff

--- SplitsLandingPage.swift
+++ SplitsLandingPage.swift
@@ -4,81 +4,92 @@ import SwiftUI
 struct SplitsLandingPageView: View {
  @State private var selectedSplit: Splits = .upper1
 
+
  var body: some View {
-     VStack {
-         Text("Select a split")
-             .font(.largeTitle)
-             .padding()
+    ZStack {
+        Color(UIColor.systemGroupedBackground) // Change this to your desired color
+            .ignoresSafeArea()
+        
+        VStack {
+            Text("Select a split")
+                .font(.largeTitle)
+                .padding()
 
-         HStack {
-             Button(action: {
-               self.selectedSplit = .upper1
-             }) {
-               Text("Upper 1")
-                  .padding()
-                  .background(Color.blue)
-                  .foregroundColor(.white)
-                  .cornerRadius(10)
-             }
-             
-             Button(action: {
-               self.selectedSplit = .upper2
-             }) {
-               Text("Upper 2")
-                  .padding()
-                  .background(Color.blue)
-                  .foregroundColor(.white)
-                  .cornerRadius(10)
-             }
-         }
+            HStack {
+                Button(action: {
+                 self.selectedSplit = .upper1
+                }) {
+                 Text("Upper 1")
+                    .padding()
+                    .background(Color.white)
+                    .foregroundColor(.blue)
+                    .cornerRadius(10)
+                }
+                
+                Button(action: {
+                 self.selectedSplit = .upper2
+                }) {
+                 Text("Upper 2")
+                    .padding()
+                    .background(Color.white)
+                    .foregroundColor(.blue)
+                    .cornerRadius(10)
+                }
+            }
 
-         HStack {
-             Button(action: {
-               self.selectedSplit = .shoulders
-             }) {
-               Text("Shoulders")
-                  .padding()
-                  .background(Color.blue)
-                  .foregroundColor(.white)
-                  .cornerRadius(10)
-             }
-             
-             Button(action: {
-               self.selectedSplit = .legs
-             }) {
-               Text("Legs")
-                  .padding()
-                  .background(Color.blue)
-                  .foregroundColor(.white)
-                  .cornerRadius(10)
-             }
-         }
-         
-         // Use a switch statement to present the correct view based on the selectedSplit variable
-         switch selectedSplit {
-         case .upper1:
-             Text("Upper 1 Exercises")
-                 .font(.system(size:24))
-                .padding()
-             Upper1ListView()
-         case .upper2:
-             Text("Upper 2 Exercises")
-                 .font(.system(size:24))
-                .padding()
-             Upper2ListView()
-         case .shoulders:
-             Text("Shoulders Exercises")
-                 .font(.system(size:24))
-                .padding()
-             ShouldersListView()
-         case .legs:
-             Text("Legs Exercises")
-                 .font(.system(size:24))
-                .padding()
-             LegsListView()
-         default:
-             EmptyView()
-         }
-     }
+            HStack {
+                Button(action: {
+                 self.selectedSplit = .shoulders
+                }) {
+                 Text("Shoulders")
+                    .padding()
+                    .background(Color.white)
+                    .foregroundColor(.blue)
+                    .cornerRadius(10)
+                }
+                
+                Button(action: {
+                 self.selectedSplit = .legs
+                }) {
+                 Text("Legs")
+                    .padding()
+                    .background(Color.white)
+                    .foregroundColor(.blue)
+                    .cornerRadius(10)
+                }
+            }
+            
+            // Use a switch statement to present the correct view based on the selectedSplit variable
+            switch selectedSplit {
+            case .upper1:
+                Text("Upper 1 Exercises")
+                  .font(.system(size:24))
+                 .padding()
+                 .offset(y:20)
+                 .underline()
+                Upper1ListView()
+            case .upper2:
+                Text("Upper 2 Exercises")
+                  .font(.system(size:24))
+                 .padding()
+                 .underline()
+                Upper2ListView()
+            case .shoulders:
+                Text("Shoulders Exercises")
+                  .font(.system(size:24))
+                 .padding()
+                 .underline()
+                ShouldersListView()
+            case .legs:
+                Text("Legs Exercises")
+                  .font(.system(size:24))
+                 .padding()
+                 .underline()
+                LegsListView()
+            default:
+                EmptyView()
+            }
+        }
+    }
  }
 }
--- Track Dat/ContentView.swift
+++ Track Dat/ContentView.swift
@@ -8,110 +8,137 @@
 import Foundation
 import SwiftUI
 
-
 struct SplitsView: View {
  @EnvironmentObject var splitsViewModel: SplitsViewModel
  @Binding var selectedSplit: Int
 
  var body: some View {
  NavigationView {
-     TabView(selection: $selectedSplit) {
-         Upper1ListView()
-             .tabItem {
-               Label("Upper 1", systemImage: "1.square")
-             }.tag(0)
-         
-         Upper2ListView()
-             .tabItem {
-               Label("Upper 2", systemImage: "2.square")
-             }.tag(1)
-         
-         ShouldersListView()
-             .tabItem {
-               Label("Shoulders", systemImage: "3.square")
-             }.tag(2)
-         
-         LegsListView()
-             .tabItem {
-               Label("Legs", systemImage: "4.square")
-             }.tag(3)
-     }
-     .navigationTitle("Splits")
-     .onAppear {
-         print("Fetching Upper1")
-         splitsViewModel.fetchUpper1()
-         print("Fetching Upper2")
-         splitsViewModel.fetchUpper2()
-         print("Fetching Shoulders")
-         splitsViewModel.fetchShoulders()
-         print("Fetching Legs")
-         splitsViewModel.fetchLegs()
-     }
-     .onReceive(splitsViewModel.$upper1) { _ in
-     }
-     .onReceive(splitsViewModel.$upper2) { _ in
-     }
-     .onReceive(splitsViewModel.$shoulders) { _ in
-     }
-     .onReceive(splitsViewModel.$legs) { _ in
-     }
+    TabView(selection: $selectedSplit) {
+        Upper1ListView()
+            .tabItem {
+             Label("Upper 1", systemImage: "1.lane")
+            }.tag(0)
+        
+        Upper2ListView()
+            .tabItem {
+             Label("Upper 2", systemImage: "2.lane")
+            }.tag(1)
+        
+        ShouldersListView()
+            .tabItem {
+             Label("Shoulders", systemImage: "3.lane")
+            }.tag(2)
+        
+        LegsListView()
+            .tabItem {
+             Label("Legs", systemImage: "4.lane")
+            }.tag(3)
+    }
+    .toolbar {
+        ToolbarItem(placement: .principal) {
+            HStack {
+               Spacer()
+               Text(getTitle())
+                    .font(.system(size: 30))
+                    .offset(y: 30)
+                    .underline()
+               Spacer()
+            }
+        }
+    }
+    .onAppear {
+        print("Fetching Upper1")
+        splitsViewModel.fetchUpper1()
+        print("Fetching Upper2")
+        splitsViewModel.fetchUpper2()
+        print("Fetching Shoulders")
+        splitsViewModel.fetchShoulders()
+        print("Fetching Legs")
+        splitsViewModel.fetchLegs()
+    }
+    .onReceive(splitsViewModel.$upper1) { _ in
+    }
+    .onReceive(splitsViewModel.$upper2) { _ in
+    }
+    .onReceive(splitsViewModel.$shoulders) { _ in
+    }
+    .onReceive(splitsViewModel.$legs) { _ in
+    }
+ }
+ }
+
+ func getTitle() -> String {
+ switch selectedSplit {
+ case 0:
+    return "Upper 1"
+ case 1:
+    return "Upper 2"
+ case 2:
+    return "Shoulders"
+ case 3:
+    return "Legs"
+ default:
+    return ""
  }
  }
 }
 
-enum Splits: String, CaseIterable, Identifiable {
-  case upper1 = "Upper 1"
-  case upper2 = "Upper 2"
-  case shoulders = "Shoulders"
-  case legs = "Legs"
 
-  var id: Self { self }
+
+enum Splits: String, CaseIterable, Identifiable {
+ case upper1 = "Upper 1"
+ case upper2 = "Upper 2"
+ case shoulders = "Shoulders"
+ case legs = "Legs"
+
+ var id: Self { self }
 }
 
 
 struct ContentView: View {
-    @StateObject var viewModel = ViewModel()
-    @StateObject var splitsViewModel = SplitsViewModel() // StateObject for your ViewModel
-    @State private var selectedTab = 0
-    @State private var selectedSplit = 0
+  @StateObject var viewModel = ViewModel()
+  @StateObject var splitsViewModel = SplitsViewModel() // StateObject for your ViewModel
+  @State private var selectedTab = 0
+  @State private var selectedSplit = 0
 
-    var body: some View {
-        NavigationView {
-            TabView(selection: $selectedTab) {
-                HomeView()
-                    .tabItem {
-                        Label("Home", systemImage: "gym.bag.fill")
-                    }
-                    .tag(0)
+  var body: some View {
+      NavigationView {
+          TabView(selection: $selectedTab) {
+              HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "gym.bag.fill")
+                }
+                .tag(0)
 
-                SplitsLandingPageView()
-                    .tabItem {
-                        Label("Splits Landing", systemImage: "house.fill")
-                    }
-                    .tag(1)
-                    .environmentObject(splitsViewModel) // Pass the environment object
+              SplitsLandingPageView()
+                .tabItem {
+                    Label("Splits Landing", systemImage: "house.fill")
+                }
+                .tag(1)
+                .environmentObject(splitsViewModel) // Pass the environment object
 
-                ExercisesListView()
-                    .tabItem {
-                        Label("Exercises", systemImage: "list.bullet.clipboard")
-                    }
-                    .tag(2)
+              ExercisesListView()
+                .tabItem {
+                    Label("Exercises", systemImage: "list.bullet.clipboard")
+                }
+                .tag(2)
 
-                SplitsView(selectedSplit: $selectedSplit)
-                    .tabItem {
-                        Label("Splits", systemImage: "list.dash")
-                    }
-                    .tag(3)
-                    .environmentObject(splitsViewModel) // Pass the environment object
-            }
-        }
-    }
+              SplitsView(selectedSplit: $selectedSplit)
+                .tabItem {
+                    Label("Splits", systemImage: "list.dash")
+                }
+                .tag(3)
+                .environmentObject(splitsViewModel) // Pass the environment object
+          }
+      }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-            .environmentObject(SplitsViewModel()) // Providing the environmentObject for the preview
-            .environmentObject(ViewModel()) // Adding this line
-    }
+  static var previews: some View {
+      ContentView()
+          .environmentObject(SplitsViewModel()) // Providing the environmentObject for the preview
+          .environmentObject(ViewModel()) // Adding this line
+  }
 }
